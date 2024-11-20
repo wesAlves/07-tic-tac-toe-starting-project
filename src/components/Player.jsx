18b361da918999443c67885e9f8af755ad22dabd
Wes Benvindo <wal2.designer@gmail.com>
2024-11-20 11:42:21 +0000

    Starting project

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import {useState} from "react";
+
+export default function Player({name, symbol}) {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(name)
+
+    const handleEditClick = () => {
+        setIsEditing(editing => !editing)
+
+    }
+
+    const handleChange = (event) => {
+        setPlayerName(event.target.value)
+    }
+
+    let editablePlayerName = <span className='player-name'>{playerName}</span>
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/> //this is tow way biding
+    }
+
+
+    return (<li>
+                    <span className="player">
+                        {editablePlayerName}
+                        <span className="player-symbol">{symbol}</span>
+                    </span>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>)
+}
