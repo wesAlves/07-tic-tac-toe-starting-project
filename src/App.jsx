05d05b4dd4c55cfd2c36d849ac90b3c37d3cf5a6
Wes Benvindo <wal2.designer@gmail.com>
2024-11-20 12:12:25 +0000

    Assing GameBoard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
     return (<main>
@@ -7,6 +8,7 @@ function App() {
                 <Player name="Player 1" symbol="x"/>
                 <Player name="Player 2" symbol="O"/>
             </ol>
+            <GameBoard />
         </div>
     </main>)
 }
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,13 @@
+const initialGameBoard = [[null, null, null], [null, null, null], [null, null, null]]
+
+export default function GameBoard() {
+    return (<ol id={"game-board"}>
+        {initialGameBoard.map((row, rowIndex) => (<li key={rowIndex}>
+            <ol>
+                {row.map((col, colIndex) => (<li key={colIndex}>
+                    <button>{col}</button>
+                </li>))}
+            </ol>
+        </li>))}
+    </ol>)
+}
